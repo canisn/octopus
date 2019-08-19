5ad48eb2db20aa711aa2f804072c99b88e213b05
zhangdeen <zhangdeen2014@xiaochuankeji.cn>
2019-08-19 12:52:22 +0800

    reverse rank

--- core/indexer.go
+++ core/indexer.go
@@ -182,7 +182,7 @@ type PairList []Pair
 
 func (p PairList) Swap(i, j int)      { p[i], p[j] = p[j], p[i] }
 func (p PairList) Len() int           { return len(p) }
-func (p PairList) Less(i, j int) bool { return p[i].Value < p[j].Value }
+func (p PairList) Less(i, j int) bool { return p[i].Value > p[j].Value }
 func sortMapByValue(m map[uint32]float32) PairList {
 	p := make(PairList, len(m))
 	i := 0
